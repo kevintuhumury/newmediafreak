778c68f1d5b017b24736ba535ab55594b5b866f1
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2012-07-19 16:24:41 +0200

    Added a list of the latest articles.

--- features/step_definitions/article_steps.rb
+++ features/step_definitions/article_steps.rb
@@ -1,6 +1,9 @@
 Given "there are articles" do
-  @article_first  = Fabricate :article, title: "First article"
-  @article_second = Fabricate :article, title: "Second article"
+  @article_one          = Fabricate :article, title: "First article", created_at: 7.days.ago
+  @article_two          = Fabricate :article, title: "Second article", created_at: 6.days.ago
+  @article_three        = Fabricate :article, title: "Third article", created_at: 5.days.ago
+  @article_next_to_last = Fabricate :article, title: "Next to last article", created_at: 4.days.ago
+  @article_latest       = Fabricate :article, title: "Latest article", created_at: 3.days.ago
 end
 
 When "I visit the homepage" do
@@ -9,13 +12,21 @@ end
 
 Then "I should see the latest articles" do
   within "#content .articles" do
-    page.should have_selector ".article h2", text: "First article"
-    page.should have_selector ".article h2", text: "Second article"
+    within "#latest-article" do
+      page.should have_selector ".title h2", text: "Latest article"
+      page.should have_selector ".info .read-more", text: "Lees verder"
+    end
+
+    within "#next-to-last-articles" do
+      page.should have_selector ".title h2", text: "Next to last article"
+      page.should have_selector ".title h2", text: "Third article"
+      page.should have_selector ".title .read-more", text: "Lees verder", count: 2
+    end
   end
 end
 
 When "I select the latest article" do
-  pending # express the regexp above with the code you wish you had
+  pending
 end
 
 Then "I should see the full article" do
