2782e9a735579f13cc5da0a1da8de42189da905e
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2012-07-22 16:16:14 +0200

    Added a legacy route matcher.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Newmediafreak::Application.routes.draw do
   root :to => 'articles#index'
 
   match "/404", :to => "errors#not_found"
+
+  # match legacy permalinks
+  match "/:year/:month/:day/:id", to: "articles#show", constraints: { year: /\d{4}/, month: /\d{2}/, day: /\d{2}/ }
 end
