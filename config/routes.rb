7a743d5f04af746842cdfb98b78914f299940941
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2012-10-17 15:30:38 +0200

    Ruby 1.9 syntax, yo!

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Newmediafreak::Application.routes.draw do
 
   resources :articles, only: [:index, :show], path: "artikel"
-  root :to => 'articles#index'
+  root to: "articles#index"
 
-  match "/404", :to => "errors#render_error"
-  match "/500", :to => "errors#render_error"
+  match "/404", to: "errors#render_error"
+  match "/500", to: "errors#render_error"
 
   # match legacy permalinks
   match "/:year/:month/:day/:id", to: "articles#show", constraints: { year: /\d{4}/, month: /\d{2}/, day: /\d{2}/ }
