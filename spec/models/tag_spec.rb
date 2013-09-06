575947cb242244a79fe9783f8d2c216363e9934e
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-09-06 08:48:36 +0200

    Removed whitespace.

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,9 +5,7 @@ describe Tag do
   subject { Fabricate :tag }
 
   it { should have_and_belong_to_many(:articles) }
-
   it { should validate_presence_of(:name) }
-
   it { should validate_uniqueness_of(:name) }
 
   describe "#frequency" do
